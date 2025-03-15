5b1868a2afc1c3888a5fe5bd3455602e822fe9bb
Harivansh <jazzz1815@gmail.com>
2025-03-15 17:15:41 +0530

    ui improve

--- client/src/components/Generatedout.jsx
+++ client/src/components/Generatedout.jsx
@@ -5,6 +5,10 @@ import { Response } from "../context/Response.context";
 export default function Generatedout() {
     const { response } = useContext(Response);
 
+    const handleAnimationComplete = () => {
+        console.log('All letters have animated!');
+    };
+
     const copyToClipboard = () => {
         navigator.clipboard.writeText(response);
         alert("Copied to clipboard! ✅");
@@ -12,17 +16,20 @@ export default function Generatedout() {
 
 
     return (
-        <div className=" w-1/2 h-screen flex justify-start overflow-y-auto items-center box-content  rounded-4xl p-5 flex-wrap ">
+        <div className=" w-1/2 h-screen flex justify-start overflow-y-auto items-center box-content rounded-2xl p-5 flex-wrap bg-blue-800 text-white ">
             <button
                 disabled={!response}
                 onClick={copyToClipboard}
-                className=" bg-blue-500 justify-start text-white px-3 py-1 rounded top-2 right-6 mt-4 mb-2 fixed "
+                className=" bg-blue-500 justify-start text-white px-3 py-1 rounded top-2 right-6 mt-4 mb-2 fixed"
             >
                 Copy Code
             </button>
 
-            <ReactMarkdown>{response}</ReactMarkdown>
-        </div>
+            <ReactMarkdown>
+                {response}
+            </ReactMarkdown>
+
+        </div >
     );
 }
 
--- client/src/components/Takeinput.jsx
+++ client/src/components/Takeinput.jsx
@@ -2,13 +2,17 @@ import React, { useContext } from 'react'
 import axios from 'axios'
 import { Response } from '../context/Response.context'
 import GradientAiRobot from '../assets/Graident-Ai-Robot.png';
+import SplitText from "./SplitText";
 
 export default function Takeinput() {
+
+    const handleAnimationComplete = () => {
+        console.log('All letters have animated!');
+    };
+
+
     const [input, setInput] = React.useState('')
     const { response, setresponse } = useContext(Response)
-
-
-
     const handlesubmit = (e) => {
         // console.log(response)
         e.preventDefault()
@@ -22,14 +26,48 @@ export default function Takeinput() {
     }
     return (
 
-        <div className='w-1/2 h-screen flex-col box-content bg-blue-500'>
-            <img src={GradientAiRobot} alt="image not found" className='w-10 h-10 absolute' />
-            <h1 className='font-bold text-2xl content-center justify-self-center text-white'>Generete Code</h1>
+        <div className='w-1/2 h-screen flex-col box-content bg-blue-500 rounded-2xl'>
 
-            <textarea value={input} onChange={e => { setInput(e.target.value) }} className=' resize-none w-[100%] h-[500px] p-3 text-white outline-none border-3 border-white' name="code" id=""></textarea>
+            <div className='flex flex-row absolute'>
+                <img src={GradientAiRobot} alt="image not found" className='w-10 h-10 ml-1.5 ' />
+                <SplitText
+                    text="Codermaster"
+                    className="h-14 text-sm font-semi mt-3 text-center  text-white  "
+                    delay={80}
+                    animationFrom={{ opacity: 0, transform: 'translate3d(0,50px,0)' }}
+                    animationTo={{ opacity: 1, transform: 'translate3d(0,0,0)' }}
+                    easing="easeOutCubic"
+                    threshold={0.2}
+                    rootMargin="-10px"
+                    onLetterAnimationComplete={handleAnimationComplete}
+                />
 
-            <button className='w-20 h-10 text-black font-bold ml-1 mb-1 bg-white rounded-md self-center' type='submit' onClick={handlesubmit}>Generate</button>
+            </div>
+
+            <div className='h-[40px]  flex justify-center'>
+                <SplitText
+                    text="Generate code"
+                    className="h-20 text-2xl font-bold mt-2 text-center  text-white "
+                    delay={80}
+                    animationFrom={{ opacity: 0, transform: 'translate3d(0,50px,0)' }}
+                    animationTo={{ opacity: 1, transform: 'translate3d(0,0,0)' }}
+                    easing="easeOutCubic"
+                    threshold={0.2}
+                    rootMargin="-50px"
+                    onLetterAnimationComplete={handleAnimationComplete}
+                />
+
+
+            </div>
+
+            <textarea value={input} onChange={e => { setInput(e.target.value) }} placeholder='Give Me Code Master...' className=' bg-blue-900 resize-none w-[100%] h-[500px] p-3 text-white outline-none border-3 border-white' name="code" id=""></textarea>
+
+            <button className='w-20 h-10 text-black font-base ml-4 mb-1 bg-white rounded-md self-center' type='submit' onClick={handlesubmit}>Generate</button>
+
         </div>
+
+
+
 
     )
 }
