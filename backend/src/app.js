295d456446e0d8d3a32210a6608cb4a369fd4079
Harivansh Sharma <jazzz1815@gmail.com>
2025-03-16 13:32:01 +0530

    Update app.js

--- backend/src/app.js
+++ backend/src/app.js
@@ -7,7 +7,7 @@ const aiRoutes = require("../routes/ai.routes");
 
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "https://aicoderreviewer-client.onrender.com",
     methods: "GET, POST",
     credentials: true,
   })
